f0bad77df228276c7552650e4da726b797f7f6de
Li Jun LM Peng <pengljun@cn.ibm.com>
2019-11-25 17:28:24 +0800

    :art:新建项目目录

---
+++ pages/booksource/model/Book.js
@@ -0,0 +1,22 @@
+const book ={
+	bookname:'', 
+	author:'',
+	//小说目录页地址
+	url:'',
+	imageUrl:'',
+	//章节数量
+	chapterSize:'',
+	lastUpdateTime:'',
+	//最新章节名
+	lastChapterName:'',
+	//小说网站名字
+	siteName:'',
+	//小说简介
+	introduce:'',
+	//小说分类
+	classify:'',
+	//小说状态，连载，完结等
+	status:''
+}
+
+export default book;
---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{id}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import book from '@/pages/booksource/model/Book.js'
+	
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				id:book.id
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
